a790a580b504394df74f737011747bc7c3e460e5
RobertoSanchez18 <roberto.sanchez@vallegrande.edu.pe>
2023-09-01 12:29:22 -0500

    Todo List App

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent {
+editableId: number | null = null;
+
+newTask: string = '';
+
+  tasks: any[] = [
+    {
+      title: 'Crear la lista de tareas',
+      completed: true,
+    },
+    {
+      title: 'Realizar la estructura html',
+      completed: true,
+    },
+    {
+      title: 'Desplegar el proyecto en la web',
+      completed: true,
+    }
+  ];
+
+  addTask(){
+    const Task = {
+      title: this.newTask,
+      completed: false,
+    }
+    this.tasks.push(Task);
+    this.newTask = '';
+  }
+
+  updateTask(task: any, title: string) {
+    const index = this.tasks.indexOf(task);
+    const updateTask = {
+      title,
+      completed: task.completed
+    }
+    this.tasks[index] = { ...task, ...updateTask };
+  }
+
+  deleteTask(task: any) {
+    const index = this.tasks.indexOf(task);
+    this.tasks.splice(index,1);
+  }
+
+  startEdit(id: number): void {
+    this.editableId = id;
+  }
+
+  stopEdit(task: any, title: string): void {
+    this.editableId = null;
+    this.updateTask(task, title)
+  }
+
+
+}
